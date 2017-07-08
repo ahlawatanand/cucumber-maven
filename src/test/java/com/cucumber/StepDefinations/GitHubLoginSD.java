d7940a0592ac1875076ad6c8e2f7c4fa6e7793b4
ANANDHP\Anand <ahlawat.anand@gmail.com>
2017-07-09 02:01:53 +1200

    recent changes

--- src/test/java/com/cucumber/StepDefinations/GitHubLoginSD.java
+++ src/test/java/com/cucumber/StepDefinations/GitHubLoginSD.java
@@ -4,7 +4,6 @@ import java.net.MalformedURLException;
 
 import com.cucumber.Selenium.SeleniumFunctions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
